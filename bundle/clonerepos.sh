515a66370cd9a9218d4fc6ffa41a7f1a5a7f4702
Aaron Gonzales <xysmas@gmail.com>
2014-06-30 22:31:13 -0600

    added clonerepos script for getting the few github repos for vim

---
+++ bundle/clonerepos.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+linking vimrc to the .vimrc dir
+ln -sf ~/.vim/.vimrc ~/.vimrc 
+cd ~/.vim/bundle
+
+echo cloning repos
+git clone https://github.com/kien/rainbow_parentheses.vim.git
+git clone https://github.com/Lokaltog/vim-easymotion.git
+git clone https://github.com/tpope/vim-surround.git
+git clone https://github.com/tpope/vim-fugitive.git
+git clone https://github.com/vim-scripts/Conque-Shell.git
+git clone https://github.com/tpope/vim-markdown.git
+git clone https://github.com/altercation/vim-colors-solarized.git
