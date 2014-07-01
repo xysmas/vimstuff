92a4888327b43066d8b374aa19905c3d511c4258
xysmas <xysmas@gmail.com>
2014-07-01 11:21:28 -0600

    added a few things to init script

--- bundle/clonerepos.sh
+++ bundle/clonerepos.sh
@@ -5,12 +5,12 @@ ln -sf ~/.vim/.vimrc ~/.vimrc
 cd ~/.vim/bundle
 
 echo cloning repos
-git clone https://github.com/kien/rainbow_parentheses.vim.git
-git clone https://github.com/Lokaltog/vim-easymotion.git
-git clone https://github.com/tpope/vim-surround.git
-git clone https://github.com/tpope/vim-fugitive.git
-git clone https://github.com/vim-scripts/Conque-Shell.git
-git clone https://github.com/tpope/vim-markdown.git
-git clone https://github.com/altercation/vim-colors-solarized.git
-git clone https://github.com/Valloric/YouCompleteMe.git
+#git clone https://github.com/kien/rainbow_parentheses.vim.git
+#git clone https://github.com/Lokaltog/vim-easymotion.git
+#git clone https://github.com/tpope/vim-surround.git
+#git clone https://github.com/tpope/vim-fugitive.git
+#git clone https://github.com/vim-scripts/Conque-Shell.git
+#git clone https://github.com/tpope/vim-markdown.git
+#git clone https://github.com/altercation/vim-colors-solarized.git
+#git clone https://github.com/Valloric/YouCompleteMe.git
 git submodule update --init --recursive
