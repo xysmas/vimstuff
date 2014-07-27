a22b3aef9c6604000ad2a8dcc75a4514451f178e
Aaron Gonzales <xysmas@gmail.com>
2014-07-27 14:49:37 -0600

    updated vimrc

--- clonerepos.sh
+++ clonerepos.sh
@@ -3,6 +3,7 @@
 echo linking repo vimrc to the .vimrc init file
 if [ -d "$HOME/vimstuff" ]; then
 	ln -sf ~/vimstuff/.vimrc ~/.vimrc 
+	ln -s $HOME/vimstuff/ $HOME/.vim
 	if [-d "$HOME/vimstuff" ]; then
 		cd $HOME/vimstuff/bundle
 	else
